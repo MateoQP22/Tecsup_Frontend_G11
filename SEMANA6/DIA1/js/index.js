8ac3d9233273f7341996b7447f8d1966ae3efbd8
MateoQP22 <73276257@continental.edu.pe>
2022-12-12 23:17:06 -0500

    s6.1 Atrapando valores de formularios y asignando formula

--- SEMANA6/DIA1/js/index.js
+++ SEMANA6/DIA1/js/index.js
@@ -1,8 +1,18 @@
 'use strict';
 
-const documentReady = () =>{
-    alert('Documento Listo')
+const documentReady = () => {
+    const calculatorForm = document.getElementById('calculatorForm');
+
+    const calculate = (e) => {
+        e.preventDefault();
+        const number1 = document.getElementById('number1').value;
+        const operator = document.getElementById('operator').value;
+        const number2 = document.getElementById('number2').value;
+
+        alert(+number1 + +number2);
+    };
+
+    calculatorForm.addEventListener('submit', calculate);
 };
 
-document.addEventListener('DOMContentLoaded',documentReady)
-
+document.addEventListener('DOMContentLoaded', documentReady);
