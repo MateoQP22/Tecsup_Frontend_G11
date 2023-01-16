f83a9c29c2668ce000ce47f4e3db9cf0eb5293e4
MateoQP22 <73276257@continental.edu.pe>
2023-01-15 20:59:56 -0500

    Semana 10 dios mio react

--- SEMANA10/eakon-frontend/src/components/sections/Header.jsx
+++ SEMANA10/eakon-frontend/src/components/sections/Header.jsx
@@ -1,43 +1,97 @@
-import { Link } from 'react-router-dom';
-import logoEAKON from '../../assets/img/logo-eakon.webp';
+import { useRef } from 'react';
+import { BsList, BsXCircle } from "react-icons/bs";
+import { Link, NavLink } from 'react-router-dom';
+import logoEAKON from '../../assets/img/logo-eakon.png';
 
 const Header = () => {
 
-  const header = document.querySelector('select');
-  
+  const header = useRef();
+  const modal = useRef();
+
+  const showMenu = () => {
+    header.current.classList.add('header--move');
+    modal.current.classList.add('modal--show');
+  };
+
+  const hideMenu = () => {
+    header.current.classList.remove('header--move');
+    modal.current.classList.remove('modal--show');
+  };
 
   return (
-    <header className="header">
+    <header
+      className="header"
+      ref={header}
+    >
       <nav className="nav">
         <div className="container f-elements f-elements--header">
           <Link to="/">
             <img src={logoEAKON} alt="Logo de EAKON" className="nav__logo" width="32" height="32" />
           </Link>
-          
-          <div className="modal modal--header">
-            <ul className="list list--header">
+          <div
+            className="modal modal--header"
+            ref={modal}
+            onClick={hideMenu}
+          >
+            <ul
+              className="list list--header"
+              onClick={e => e.stopPropagation()}
+            >
               <li>
-                <Link to="/productos" className="list__link">Productos</Link>
+                <NavLink
+                  to="/productos"
+                  className={({ isActive }) => isActive ? "list__link list__link--active" : "list__link"}
+                  onClick={hideMenu}
+                >
+                  Productos
+                </NavLink>
               </li>
               <li>
-                <Link to="/servicios" className="list__link">Servicios</Link>
+                <NavLink
+                  to="/servicios"
+                  className={({ isActive }) => isActive ? "list__link list__link--active" : "list__link"}
+                  onClick={hideMenu}
+                >
+                  Servicios
+                </NavLink>
               </li>
               <li>
-                <Link to="/nosotros" className="list__link">Nosotros</Link>
+                <NavLink
+                  to="/nosotros"
+                  className={({ isActive }) => isActive ? "list__link list__link--active" : "list__link"}
+                  onClick={hideMenu}
+                >
+                  Nosotros
+                </NavLink>
               </li>
               <li>
-                <Link to="/contacto" className="list__link">Contacto</Link>
+                <NavLink
+                  to="/contacto"
+                  className={({ isActive }) => isActive ? "list__link list__link--active" : "list__link"}
+                  onClick={hideMenu}
+                >
+                  Contacto
+                </NavLink>
+              </li>
+              <li
+                className="icon icon--close"
+                onClick={hideMenu}
+              >
+                <BsXCircle />
               </li>
             </ul>
           </div>
           <div className="f-elements f-elements--center">
-            <select>
-              <option value="value1">🌑</option>
-              <option value="value2">☀</option>
+            {/* <select>
+              <option value="value1">🌚</option>
+              <option value="value2">🌞</option>
               <option value="value3">💻</option>
-            </select>
-            <button className="icon">
-              🍔
+            </select> */}
+            <button
+              className="icon icon--menu"
+              onClick={showMenu}
+            >
+              <BsList />
             </button>
           </div>
         </div>
